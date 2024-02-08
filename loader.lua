b4ee31fb18cf85ca0fbfc0b8cbcdc18ece12d728
bananawithextendedclip <159469222+bananawithextendedclip@users.noreply.github.com>
2024-02-08 17:33:59 +0000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/bananawithextendedclip/Liberation/main/reall/LiberationV1.lua?token=GHSAT0AAAAAACN7FGE4DZMKK73JYTLKNWMYZOFBPZQ"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/bananawithextendedclip/Liberation/main/reall/LiberationV1.lua"))()
